4133f93687875052ed4f05aeeb17ffd2fa004226
remon351 <remon7105@gmail.com>
2024-11-30 14:01:30 +0200

    add validation

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac60565c604462c0ae97cf3ebae8b001f2fa2750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19c7bc41cb953b028e88988c6c8fe1530b82b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
